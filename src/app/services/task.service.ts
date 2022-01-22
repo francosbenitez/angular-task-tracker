b38ae1cce022b84142b3e0029c4db018788787c6
francobenitez <franco.benitez@hogarthww.com>
2022-01-22 17:39:12 -0300

    feat: add functionality for deleting tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs'
+// import { Observable, of } from 'rxjs'
+import { Observable } from 'rxjs'
 import { Task } from '../Task'
-import { TASKS } from '../mock-tasks'
+// import { TASKS } from '../mock-tasks'
+
+// This is only part of Angular. It has an HTTP client included
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   // We create a function here
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    // const tasks = of(TASKS)
+    // return tasks
+    return this.http.get<Task[]>(this.apiUrl)
   }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
 }
